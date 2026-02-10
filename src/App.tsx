658f03e445183a1956b6222c06ef6cf15de205f9
Søren Anton Smidt <au754363@uni.au.dk>
2026-02-10 20:27:33 +0100

    Started adding UI for importing strava segment.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 export default function App() {
     return (
-        <h1 className="text-3xl font-bold text-red-400 underline">
-            Hello EiN!
-        </h1>
+        <div className="flex flex-col items-center">
+            <input
+                type="text"
+                placeholder="https://www.strava.com/segments/37032431"
+                className="
+                    bg-primary border-1 border-primary-shade rounded-lg
+                    w-[75vw] h-[5vh] p-3
+                    outline-none
+                    focus:ring-2
+                    focus:ring-dark-shade
+                    transition-all duration-200
+                    "></input>
+        </div>
     )
 }
