5b6754f1dee2cbde6bcba1df0a0be2bb5fc6a441
Søren Anton Smidt <au754363@uni.au.dk>
2026-02-10 20:45:36 +0100

    Added temporary logo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import logo from './assets/ein.png';
+
 export default function App() {
     return (
         <div className="flex flex-col items-center">
+            <header className="
+                w-full h-[10vh] border-b-1 border-b-primary-shade
+                flex justify-center">
+                <img src={logo} alt="ein" className="h-full" />
+            </header>
             <input
                 type="text"
                 placeholder="https://www.strava.com/segments/37032431"
@@ -11,7 +18,8 @@ export default function App() {
                     focus:ring-2
                     focus:ring-dark-shade
                     transition-all duration-200
-                    "></input>
+                    ">
+            </input>
         </div>
     )
 }
