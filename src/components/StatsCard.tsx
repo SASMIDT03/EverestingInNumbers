ab4f4c443e66906275be9de61bc3b34ff972e2ac
Søren Anton Smidt <au754363@uni.au.dk>
2026-02-13 22:00:18 +0100

    Added icons and proper display text.

--- src/components/StatsCard.tsx
+++ src/components/StatsCard.tsx
@@ -1,6 +1,8 @@
 
 type StatsCardProps={
-    mainText: string
+    mainText: string,
+    iconUrl: string,
+    iconAlt: string,
 }
 
 export function StatsCard(props: StatsCardProps) {
@@ -8,7 +10,9 @@ export function StatsCard(props: StatsCardProps) {
         <div className="
             bg-secondary border-1 border-primary-shade rounded-lg
             font-semibold text-dark-2 w-full
+            p-2
             ">
+            <img src={props.iconUrl} alt={props.iconAlt} className="w-8 " />
             {props.mainText}
         </div>
     )
--- src/components/StatsDisplay.tsx
+++ src/components/StatsDisplay.tsx
@@ -1,12 +1,16 @@
 import type {EverestingStats} from "../utils/types/EverestingStats.ts";
 import {StatsCard} from "./StatsCard.tsx";
+import DistanceIcon from "../assets/distance.svg"
+import LapsIcon from "../assets/repeat.svg"
+import ClockIcon from "../assets/clock.svg"
+import EnergyIcon from "../assets/energy.svg"
 
 type StatsDisplayProps = {
     stats: EverestingStats | null
 }
 
-export function StatsDisplay(props: StatsDisplayProps) {
-    if (!props.stats) return (
+export function StatsDisplay( stats: StatsDisplayProps) {
+    if (!stats.stats) return (
         <div className="
             bg-secondary border-1 border-primary-shade rounded-lg
             p-6 m-6 w-[30vw] min-h-[51vh]
@@ -26,11 +30,10 @@ export function StatsDisplay(props: StatsDisplayProps) {
             <div className="
                 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4
                 ">
-                <StatsCard mainText={"Test1"} />
-                <StatsCard mainText={"Test1"} />
-                <StatsCard mainText={"Test1"} />
-                <StatsCard mainText={"Test1"} />
-                <StatsCard mainText={"Test1"} />
+                <StatsCard mainText={ stats.stats!.totalDistance.toString() + "km"} iconUrl={DistanceIcon} iconAlt={"Distance icon"}/>
+                <StatsCard mainText={ stats.stats!.totalNumberOfLaps.toString() } iconUrl={LapsIcon} iconAlt={"Laps icon"}/>
+                <StatsCard mainText={ stats.stats!.totalTime.toString() } iconUrl={ClockIcon} iconAlt={"Clock icon"}/>
+                <StatsCard mainText={ stats.stats!.totalEnergy.toString() + "kcal" } iconUrl={EnergyIcon} iconAlt={"Energy icon"}/>
             </div>
         </div>
     )
