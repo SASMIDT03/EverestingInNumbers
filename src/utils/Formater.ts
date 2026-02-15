61b6b98ec8bbe6caa2f19418f394cd923a44a292
Søren Anton Smidt <au754363@uni.au.dk>
2026-02-15 17:30:42 +0100

    Utility function for formating time properly.

---
+++ src/utils/Formater.ts
@@ -0,0 +1,10 @@
+export const formatTime = (timeInSeconds: number): string => {
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = timeInSeconds % 60;
+
+    if (hours > 0) {
+        return `${hours}h ${minutes}m`;
+    }
+    return `${minutes}m ${seconds}s`;
+}
