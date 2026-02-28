4ffaea195745858d0232a7cfb35a659d81a5cd4b
Søren Anton Smidt <au754363@uni.au.dk>
2026-02-28 21:29:36 +0100

    Started implementing a modal for displaying in depth stats about time.

---
+++ src/components/modals/TimeModal.tsx
@@ -0,0 +1,9 @@
+export function TimeModal() {
+    return (
+        <div
+            onClick={(e) => {e.stopPropagation()}}
+            className="bg-primary  rounded-lg">
+            Hej
+        </div>
+    )
+}
