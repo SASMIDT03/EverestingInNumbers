86ffc23ec36003b0b91017d0cb47650f86d43db5
Søren Anton Smidt <au754363@uni.au.dk>
2026-03-01 20:18:46 +0100

    Added styling when a Card is clickable.

--- src/components/StatsCard.tsx
+++ src/components/StatsCard.tsx
@@ -1,3 +1,4 @@
+import Click from "../assets/click.svg"
 
 type StatsCardProps={
     mainText: string,
@@ -6,17 +7,27 @@ type StatsCardProps={
     onClick?: () => void,
 }
 
-export function StatsCard(props: StatsCardProps) {
+function StatsCard(props: StatsCardProps) {
+
+    const hover = props.onClick ? "cursor-pointer hover:bg-primary-shade" : null
+
+
     return (
         <div
             onClick={props.onClick}
-            className="
+            className={`
             bg-secondary border-1 border-primary-shade rounded-lg
             font-semibold text-dark-2 w-full
             p-2
-            ">
-            <img src={props.iconUrl} alt={props.iconAlt} className="w-8 "/>
+            ${hover}  `}>
+            <div className="flex flex-row justify-between">
+                <img src={props.iconUrl} alt={props.iconAlt} className="w-8 "/>
+                {props.onClick && (<img src={Click} alt={props.iconAlt} className="w-4 "/>)}
+            </div>
+
             {props.mainText}
         </div>
     )
 }
+
+export default StatsCard
